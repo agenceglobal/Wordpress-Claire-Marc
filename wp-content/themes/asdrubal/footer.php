da6fedb7fdd0458babc99c89dee711b1a807a9ab
marccarrasque <116455837+marccarrasque@users.noreply.github.com>
2023-02-06 20:12:09 +0100

    Creación footer

---
+++ wp-content/themes/asdrubal/footer.php
@@ -0,0 +1,13 @@
+        <footer>
+            <address>
+                Email: <a href="info@agenciaclairemarc.com">info@agenciaclairemarc.com</a><br>
+                Barcelona, España
+            </address>
+            <p>
+                (c) 2023 Agencia de Marketing de Claire y Marc</a><br>
+            </p>
+            <p><?php echo "Web desarrollada para el Máster de SEO Técnico";?></p>
+            <?php wp_footer();?>
+        </footer>
+    </body>
+</html>
