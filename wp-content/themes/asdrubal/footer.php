1dc59bd3a43170504dfdd6e3d5dfdfb3e22d1067
marccarrasque <116455837+marccarrasque@users.noreply.github.com>
2023-02-07 20:56:50 +0100

    Mejoras footer

--- wp-content/themes/asdrubal/footer.php
+++ wp-content/themes/asdrubal/footer.php
@@ -3,12 +3,14 @@
                 Email: <a href="mailto:info@agenciaclairemarc.com">info@agenciaclairemarc.com</a><br>
                 Barcelona, España
             </address>
-            <p>
-                Aviso Legal | Política de Privacidad | Política de Cookies
-            </p>
-            <p>
-                © 2023 Agencia de Marketing de Claire y Marc | <?php echo "Web desarrollada para el Máster de SEO Técnico";?>
-            </p>
+            <div class="flexcenter">
+                <p>
+                    Aviso Legal | Política de Privacidad | Política de Cookies
+                </p>
+                <p>
+                    © 2023 Agencia de Marketing de Claire y Marc | <?php echo "Web desarrollada para el Máster de SEO Técnico";?>
+                </p>
+            </div>
             <?php wp_footer();?>
         </footer>
     </body>
