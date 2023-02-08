ff96ac148bba5b4397e616123cc751a65b9c5900
Claire <clairehernandezespin@gmail.com>
2023-02-08 20:41:11 +0100

    1ª parte tema personalizado

---
+++ wp-content/themes/asdrubal/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css" type='text/css' media='all'>
+<?php /*
+        <link href="/css/fonts.css" rel="stylesheet">
+        
+        <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST'];?>/css/estilo.css">
+
+        <?php include_once $_SERVER['DOCUMENT_ROOT'].'/assets/functions.php';
+  */ 
+wp_head();
+ ?>
+       
+
+    </head>
+<body>
+    <header>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/seo-tecnico.php">Claire</a></li>
+            <li><a href="/contactar.php">Contacto</a></li>
+            <li><a href="/ejemplo.php">Ejemplo</a></li>
+            <li><a href="/basica.php">Básica</a></li>
+            <li><a href="https://www.agenceglobalweb.com/" target="_blank">Mi web</a></li>
+         </ul>
+         <div class="cerrar">X</div>
+    </nav>
+    </header>
