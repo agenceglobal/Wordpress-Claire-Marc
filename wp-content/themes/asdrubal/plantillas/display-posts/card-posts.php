e251837a33e3241de6675ed61e5d5d77f0deffcb
marccarrasque <116455837+marccarrasque@users.noreply.github.com>
2023-02-09 17:13:47 +0100

    Estilos

--- wp-content/themes/asdrubal/plantillas/display-posts/card-posts.php
+++ wp-content/themes/asdrubal/plantillas/display-posts/card-posts.php
@@ -1,5 +1,6 @@
-<div>
-
-Esto es una llamada al card post
-
++<div class="post-card">
+    <a href="<?php the_permalink();?>" title="<?php the_permalink();?>" id="<?php the_ID();?>">
+        <div class="post-name"><?php the_title();?></div>
+        <div class="post-desc"><?php the_excerpt('Más información...');?></div>
+    </a>
 </div>
