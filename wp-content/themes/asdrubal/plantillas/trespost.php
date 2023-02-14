5d812ce8f73aa15b26ceb7d18b49aa08338ad091
Claire Hernández <104128353+agenceglobal@users.noreply.github.com>
2023-02-14 23:34:55 +0100

    Categorías y posts
    Hay dos categorías creadas: posicionamiento web y social media
    con dos post de posicionamiento web

--- wp-content/themes/asdrubal/plantillas/trespost.php
+++ wp-content/themes/asdrubal/plantillas/trespost.php
@@ -4,7 +4,7 @@ $args = array(
     'posts_per_page' => 3, /* Poner -1 para infinito */
     'post_type' => 'post',
     'orderby' => 'date', /* Poner rand para que los coja de forma aleatoria */
-    'order' => 'DESC', /* Poner ASC para que el orden sea ascendiente */
+    'order' => 'ASC', /* Poner ASC para que el orden sea ascendiente */
 );
 $the_query = new WP_Query($args);
 // Un loop
