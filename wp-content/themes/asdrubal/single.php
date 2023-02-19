1660a1273e63633b42488361a6c5129b27b852e5
Claire Hernández <104128353+agenceglobal@users.noreply.github.com>
2023-02-19 11:23:08 +0100

    campo personalizado y condicional

--- wp-content/themes/asdrubal/single.php
+++ wp-content/themes/asdrubal/single.php
@@ -2,6 +2,16 @@
 
 <h1><?php the_title();?></h1>
 
+<?php
+if (get_field('descripcion')){
+    ?>
+<div class="descripcion"><?php the_field( 'descripcion' ); ?></div>
+    <?php
+}
+
+else {echo "<img src='/wp-content/imagenes/agencia-marketing-digital.jpg'>";
+}
+?>
 <?php the_content();?>
 
 <?php get_footer();?>
