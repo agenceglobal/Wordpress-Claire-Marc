17a11feff98c294ad0efcf8d953e249f9359b99e
Claire Hernández <104128353+agenceglobal@users.noreply.github.com>
2023-02-20 07:28:10 +0100

    Información Adicional
    Incluyo códigos del apartado de información adicional pero no sé si son todos necesarios.

--- wp-content/themes/asdrubal/functions.php
+++ wp-content/themes/asdrubal/functions.php
@@ -8,3 +8,21 @@ function tresposts(){
 add_shortcode('lastest_posts', 'tresposts') // El shortcode se llama así: [lastest_posts]
 
 ?>
+
+<?php
+
+ //borrar canonical por defecto
+remove_action('wp_head', 'rel_canonical');
+
+/** Remove Canonical URL **/
+remove_action('embed_head', 'rel_canonical');
+add_filter('wpseo_canonical', '__return_false');
+
+//Eliminar meta robots
+
+remove_filter( 'wp_robots', 'wp_robots_max_image_preview_large' );
+
+//ELIMINAR EL SITEMAP POR DEFECTO DE WORDPRESS
+add_filter( 'wp_sitemaps_enabled', '__return_false' );
+
+?>
