5fac5591b3a50316c86b8c96a10f2854dbd8e229
Claire <clairehernandezespin@gmail.com>
2023-06-15 12:38:09 +0200

    cambios varios

---
+++ wp-content/plugins/clase-sitemap/test.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Lección de Sitemap
+Plugin URI:
+Description: Ejercicio para la clase.
+Author: TUNOMBRE
+Author URI: https://carlos.sanchezdonate.com
+Version: 0.1
+License: GPLv2
+*/
+
+
+//borrar el sitemap nativo de worpress
+include'includes/borrar-sitemap.php';
+//llamamos a generar plantilla
+include'includes/template-generator.php';
+?>
